131a4272c4d4be2350c1a7175bdb573452ff256c
DX-0w0 <dong7777xia@gmail.com>
2025-04-18 10:27:38 -0400

    4-18

--- 05-cache-project/Notes.md
+++ 05-cache-project/Notes.md
@@ -14,3 +14,15 @@
 - docs `https://nextjs.org/docs/app/building-your-application/caching`
 - nextjs 14 uses default `cache: 'force-cache',`
 - nextjs 15 uses default `cache: 'no-store',`
+- setting to control the cache inside the option of fetch
+- there are 2 reserved variable that needs to be exported to no cache
+  ```
+  export const revalidate = 5 // same as next: { revalidate: 5 }
+  export const dynamic = 'force-dynamic' // same as cache: 'no-store'
+  ```
+![Build route](public/route.png)
+- adding a no cache to the route of messages, in build there is a different symbol instead of a circle
+- using `revalidatePath()` is only on demand to revalid the cache
+- using `revalidateTag()` clear those pages that has those tags, set in config option in fetch
+- having `import { cache } from 'react'` inside the local database source, prevent duplicate call
+- -having `import { unstable_cache as nextCache } from 'next/cache'` store the data cache, its an async fn
--- 05-cache-project/app/messages/page.js
+++ 05-cache-project/app/messages/page.js
@@ -1,4 +1,10 @@
+import { unstable_noStore } from 'next/cache'
+
 import Messages from '@/components/messages'
+import { getMessages } from '@/lib/messages'
+
+// export const revalidate = 5 // same as next: { revalidate: 5 }
+// export const dynamic = 'force-dynamic' // same as cache: 'no-store'
 
 export default async function MessagesPage() {
   // const response = await fetch('http://localhost:8080/messages', {
@@ -8,15 +14,20 @@ export default async function MessagesPage() {
   // });
 
   // const response = await fetch('http://localhost:8080/messages', {
-  //   // cache: 'no-store', // do not cache
-  //   // cache: 'force-cache' // always cache
-  //   // next: {
-  //   //   revalidate: 5 // use cache data for _seconds then revalidate
-  //   // }
+  //   cache: 'no-store', // do not cache
+  //   cache: 'force-cache' // always cache
+  //   next: {
+  //     revalidate: 5 // use cache data for _seconds then revalidate
+  //     tags: ['msg']
+  //   }
   // })
 
-  const response = await fetch('http://localhost:8080/messages')
-  const messages = await response.json()
+  // unstable_noStore()
+
+  // const response = await fetch('http://localhost:8080/messages')
+  // const messages = await response.json()
+
+  const messages = await getMessages()
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>
