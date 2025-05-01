69d85bdf00db4d63a54d2ee31af677ef83c2125b
DX-0w0 <dong7777xia@gmail.com>
2025-05-01 15:30:39 -0400

    5-1

---
+++ pages-router/09-working-w-file-based/pages/events/[...slug].js
@@ -0,0 +1,9 @@
+function FilteredEventsPage() {
+  return (
+    <div>
+      <h1>Filterered Events Page </h1>
+    </div>
+  )
+}
+
+export default FilteredEventsPage
---
+++ pages-router/09-working-w-file-based/pages/index.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+        <li>
+          <Link href='/events'>Events</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default HomePage
